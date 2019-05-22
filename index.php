dc1e0074de75a25496f25fcc2aca4404f0ee8e20
guilherme <guilerme1041@gmail.com>
2019-05-22 17:20:19 -0300

    att 1

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+</head>
+<body>
+
+<?php
+
+    
+
+    echo "<h1>ola</h1>";
+    var_dump ("guileume ");
+
+    $idade = 0 ;
+
+    $nome = "<br>guilherme v2 <br> ";
+
+    echo $nome;
+
+    if($idade == 0){
+        echo "certo <br>";
+    }else{
+        echo "errado <br>";
+
+
+    }
+
+
+
+
+    for($i=0 ; $i <= 10 ; $i++){
+        echo "aki" . "<br>" ;
+    }
+
+?>
+    
+</body>
+</html>
