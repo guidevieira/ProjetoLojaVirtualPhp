ef785c007fdd5456d70e6ba456c2d57ff6cb9980
guilherme <guilerme1041@gmail.com>
2019-05-27 17:39:31 -0300

    att 04

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php 
-$usuario = ["logado" => false,"nome"=>"guilherme","nivelAcesso"=> 0];
+$usuario = ["logado" => true,"nome"=>"guilherme","nivelAcesso"=> 0];
 
+$produtos = [
+    "produto1"=>["nome" => "cursoFullStack",
+                "descricao" => "o curso encina programacao",
+                "preco"=>1200, "img" => "img/img3.jpg"],
 
+    "produto2"=>["nome" => "cursoAndroid",
+                  "descricao" => "o curso encina a fazer app",
+                  "preco"=>1400, "img" => "img/img2.jpg"],
+    "produto3"=>["nome" => "cursoApple",
+                  "descricao" => "o curso encina a fazer app Para Ios",
+                  "preco"=>2100 , "img" => "img/img3.jpg"],
+    "produto4"=>["nome" => "cursoApple",
+                  "descricao" => "o curso encina a fazer app Para Ios",
+                  "preco"=>2100 , "img" => "img/img2.jpg"],
+
+                  
+];
+
+$categorias = [
+    "curso",
+    "tutorias",
+    "artigos",
+    "forum",
+    "codigos"
+];
 
 
 ?>
@@ -18,7 +42,7 @@ $usuario = ["logado" => false,"nome"=>"guilherme","nivelAcesso"=> 0];
 <header>
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="index.php">Loja</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -42,10 +66,22 @@ $usuario = ["logado" => false,"nome"=>"guilherme","nivelAcesso"=> 0];
                             </li>
                     <?php else:?>
                             <li class="nav-item">
-                                <a class="nav-link" href="login.php">login</a>
+                                <a class="nav-link" href="login.php"  >login</a>
                             </li>
+                    
                     <?php endif;?>
         </div>
+
+       
+    </nav>
+    <nav>
+            <ul class="row mt-3 justify-content-center">
+                <?php foreach($categorias as $categoria):?>
+                <li class="col-md-2">
+                    <?=$categoria?>
+                </li>
+                    <?php endforeach;?>
+            </ul>
     </nav>
 
 </header>
@@ -53,42 +89,63 @@ $usuario = ["logado" => false,"nome"=>"guilherme","nivelAcesso"=> 0];
     <main class="container mt-5">
         <section class="row">
         <!--Coluna para segura -->
-            <div class="col-md-4">
+        <?php  foreach($produtos as $chave => $produto){?>
+            <div class="col-md-4 mt-3">
                 <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="<?php echo $produto["img"];?>" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title"><?php  echo $produto["nome"]; ?></h5>
+                        <p class="card-text"><?php  echo $produto["descricao"]; ?></p>
+                        <h4 class="text-success">R$<?php  echo $produto["preco"]; ?></h4>
+                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#<?php echo $chave ?>">
+                        Compra
+                        </button>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-
+        <?php }?>
         </section>
-
-
     </main>
+
+
+<!-- Modal -->
+<?php foreach($produtos as $chave=> $produto):?>
+<div class="modal fade" id="<?php echo $chave ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Produto: <?php  echo $produto["nome"]; ?></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="">
+            <div class="form-group">
+                <input type="text" name="nomeCliente" placeholder="nome completo">
+            </div>
+            <div class="form-group">
+                <input type="number" name="cpfCliente" placeholder="CPF">
+            </div>
+            <div class="form-group">
+                <input type="number" name="cartaoCliente" placeholder="Cartao De Credito">
+            </div>
+            <div class="form-group">
+                <input type="date" name="DataValidadeCartao" placeholder="Valida Do Credito">
+            </div>
+            <div class="form-group">
+                <input type="number" maxlength="3" name="cvvCartao" placeholder="CVV Do Credito">
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <h4 class="text-sucess">preço total: R$<?php  echo $produto["preco"]; ?></h4>
+        <button type="button" class="btn btn-success">Finaliza Compra2</button>
+      </div>
+    </div>
+    <?php endforeach?>
+  </div>
+</div>
         
 
 
--- login.php
+++ login.php
@@ -12,6 +12,41 @@
 </head>
 <body>
 
+<header>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.php">Loja</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <?php if(isset($usuario) && $usuario != "" &&  $usuario["logado"]):?>
+                    <?php  if($usuario["nivelAcesso"] == 0): ?>
+                            <li class="nav-item active">
+                                <a class="nav-item nav-link active" href="#">Ações <span class="sr-only">(current)</span></a>
+                            </li>
+                    <?php 
+                    else:
+                    ?>
+                            <li class="nav-item active">
+                                <a class="nav-item nav-link active" href="#">Perfil <span class="sr-only">(current)</span></a>
+                            </li>
+
+                    <?php endif;?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"> ola <?php echo $usuario["nome"]; ?> </a>
+                            </li>
+                    <?php else:?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="login.php">login</a>
+                            </li>
+                    <?php endif;?>
+        </div>
+    </nav>
+
+</header>
+
             <form class="form-signin">
         <div class="text-center mb-4">
             <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
