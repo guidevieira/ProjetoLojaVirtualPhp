662372d38f83926f54d3512bad6d5cb11ed1203d
guilherme <guilerme1041@gmail.com>
2019-05-31 17:36:13 -0300

    att 06

--- index.php
+++ index.php
@@ -31,7 +31,46 @@
 
 <!-- Modal -->
 
-<?php include "modal.php"?>
+
+
+<?php foreach($produtos as $chave=> $produto):?>
+        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Produto: <?php  echo $produto["nome"]; ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="sucesso.php">
+                    <div class="form-group">
+                        <input type="texto" name="nomeProduto" value="<?php echo $produto["nome"]?>" hidden>
+                        <input type="text" name="nomeCliente" placeholder="nome completo">
+                    </div>
+                    <div class="form-group">
+                        <input type="number" name="cpfCliente" placeholder="CPF">
+                    </div>
+                    <div class="form-group">
+                        <input type="number" name="cartaoCliente" placeholder="Cartao De Credito">
+                    </div>
+                    <div class="form-group">
+                        <input type="date" name="DataValidadeCartao" placeholder="Valida Do Credito">
+                    </div>
+                    <div class="form-group">
+                        <input type="number" maxlength="3" name="cvvCartao" placeholder="CVV Do Credito">
+                    </div>
+                    <div class="modal-footer">
+                        <h4 class="text-sucess">preço total: R$<?php  echo $produto["preco"]; ?></h4>
+                        <button type="submit" class="btn btn-primary" >Finaliza Compra2</button>
+                    </div>
+            </div>
+            </form>
+            </div>
+<?php endforeach?>
+        </div>
+        </div>
         
 
 
